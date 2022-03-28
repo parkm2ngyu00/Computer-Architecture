9811521ec64c31269f9e72c52c57d6f85e8f400f
parkmingue00 <parkm2ngyu00@naver.com>
2022-03-28 11:15:06 +0900

    change directory

---
+++ 다항식의 덧셈/instructions.c
@@ -0,0 +1,25 @@
+long ft_add(long x, long y)
+{
+    return (x + y);
+}
+
+long ft_sub(long x, long y)
+{
+    return (x - y);
+}
+
+long ft_mul(long x, long y)
+{
+    return (x * y);
+}
+
+long ft_div(long x, long y)
+{
+    return (x / y);
+}
+
+long ft_M(long reg, long x)
+{
+    reg = x;
+    return reg;
+}
